5c981b716c6228545454b6ff9d37852a9dc4db00
bhanu <bhanuu688@gmail.com>
2024-01-16 22:54:19 +0530

    com

--- src/main/java/com/infinite/TastifyApplication.java
+++ src/main/java/com/infinite/TastifyApplication.java
@@ -2,8 +2,9 @@ package com.infinite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class}) 
 public class TastifyApplication {
 
 	public static void main(String[] args) {
